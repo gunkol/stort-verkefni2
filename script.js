eda6d10d4eac6652e05954a75ebb9b7dc64fc93f
gunkol <gunnark13@gmail.com>
2017-11-23 16:13:41 +0000

    script.js grunnur kominn inn
    með bullum breytuheitum og shit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const A = 1;
+document.addEventListener('DOMContentLoaded', function () {
+  var foo = document.querySelector('foo');
+
+  program.init(foo);
+});
+
+const program = (function() {
+  const bar;
+
+  function init(foo) {
+    // const bar = foo.querySelector('');
+  }
+
+  return {
+    init: init
+  }
+})();
