2e772cbdec945841d2fcaa5cf6e29fa9e289af18
weekend37 <helgi37@gmail.com>
2017-11-24 09:21:14 +0000

    litid

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const A = 1;
+  //Eyddi 120 min i ad reyna ad lesa json skranna. nadi thvi ekki.
+  //uppastunga ad framhaldi: fa sendan koda sem les inn json skrar
+  //Annarn gaeti verid eh ad finna i verkefni 9
+
+  var strengur ='/videos.json';
+  var request = new XMLHttpRequest();
+  request.open('GET', strengur, true);
+
+  //var strengur ='/videos.json';
+  console.log(strengur);
