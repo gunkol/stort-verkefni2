6f1e1ef2c365a74af5c64a654559a4debbee9353
weekend37 <helgi37@gmail.com>
2017-11-27 18:44:58 +0000

    næstum load

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
 class VideoPlayer {
   constructor() {
-    this.keyName = 'countdown';
-    this.container = document.querySelector('.countdown');
-    this.form = document.querySelector('form');
+    this.container = document.querySelector('.videos');
     // til þess að submit hafi þennan klasa sem "this" verðum við
     // að nota bind hér (og í öðrum föllum sem við bindum!)
-    this.form.addEventListener('submit', this.submit.bind(this));
   }
+
+  load() {
+    console.log('load fallið virkar');
+    let data;
+    const request = new XMLHttpRequest();
+    request.open('GET', './videos.json', true);
+
+    request.onload = function () {
+      data = JSON.parse(request.response);
+      console.log(data);
+      // console.log(data.videos[0].id);
+      // console.log(data.categories);
+    }
+    request.send();
+    this.createCategories(data);
+  }
+
+  createCategories(data) {
+    console.log(data);
+    //this.data = data;
+    console.log('hæ');
+  }
+
+  // key: 'onLoad',
+  // value: function onLoad() {
+  //   var loading = document.createElement('h2');
+  //   loading.classList.add('text');
+  //   loading.appendChild(document.createTextNode('Hleð upplýsingum...'));
+  //   this.categories.appendChild(loading);
+  // }
+
 }
 
 class VideoPage {
@@ -16,11 +44,14 @@ class VideoPage {
 
 document.addEventListener('DOMContentLoaded', function () {
   const URL = document.URL.toString();
-  if (URL.indexOf('player.html') !== -1) {
-    var videoplayer = new VideoPlayer();
+
+
+  console.log(URL);
+  if (URL.indexOf('localhost:3000/') !== -1) {
+    const videoplayer = new VideoPlayer();
     videoplayer.load();
   } else {
-    var videopage = new VideoPage();
-    videos.load();
+    const videopage = new VideoPage();
+    videopage.load();
   }
 });
