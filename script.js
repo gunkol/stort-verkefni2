676ab3c122d35f0b345982d9e1b66e4a14d6c540
gunkol <gunnark13@gmail.com>
2017-11-27 15:28:38 +0000

    grunnur enn og aftur

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ class VideoPlayer {
 
 }
 
-class VideoPage 
+class VideoPage {
 
-  document.addEventListener('DOMContentLoaded', function () {
+}
 
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  const URL = document.URL.toString();
+  if (URL.indexOf('player.html') !== -1) {
+    var videoplayer = new VideoPlayer();
+    videoplayer.load();
+  } else {
+    var videopage = new VideoPage();
+    videos.load();
+  }
+});
