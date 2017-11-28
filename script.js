1c7226a3fc7c41a0d619672f5d4f9631c9d6fce5
gunkol <gunnark13@gmail.com>
2017-11-28 00:36:02 +0000

    videoplayer fyrsta try

--- script.js
+++ script.js
@@ -25,27 +25,71 @@ class VideoPage {
     //const categories = data.categories;
     //console.log(categories);
   }
-
-  // key: 'onLoad',
-  // value: function onLoad() {
-  //   var loading = document.createElement('h2');
-  //   loading.classList.add('text');
-  //   loading.appendChild(document.createTextNode('Hleð upplýsingum...'));
-  //   this.categories.appendChild(loading);
-  // }
-
 }
 
 class VideoPlayer {
+  constructor() {
+    const player = document.querySelector('.player');
+    const content = document.querySelector('.content');
+  }
 
+  load() {
+    const request = new XMLHttpRequest();
+    request.open('GET', './videos.json', true);
+
+    request.onload = () => {
+      this.data = JSON.parse(request.response);
+      this.createPlayer();
+    };
+    request.send();
+  }
+
+  createPlayer() {
+    // const videoId = window.location.search;
+    const videoId = 'notendur.hi.is/guk43/vefforritun/stort-verkefni2/player.html?id=2';
+    const id = videoId[videoId.length - 1];
+    // this.player = document.querySelector('.player');
+    this.content = document.querySelector('.content');
+
+    if (!this.data.videos[id]) {
+      this.error();
+    } else {
+      // debugger;
+      const titleEl = document.querySelector('title');
+      titleEl.appendChild(document.createTextNode(this.data.videos[id - 1].title));
+
+      const title = document.createElement('h1');
+      title.classList.add('text', 'text__spilaraTitill');
+      title.appendChild(document.createTextNode(this.data.videos[id - 1].title));
+      // this.player.appendChild(title);
+      this.content.appendChild(title);
+
+      const videoBox = document.createElement('div'); // videoContainer
+      videoBox.classList.add('videoBox');
+      // this.player.appendChild(videoBox);
+      this.content.appendChild(videoBox);
+
+      const src = this.data.videos[id - 1].video;
+      const video = document.createElement('video');
+      video.classList.add('video');
+      video.src = src;
+      videoBox.appendChild(video);
+      this.player.appendChild(videoBox);
+    }
+  }
+
+  error() {
+    const titleEl = document.querySelector('title');
+    titleEl.appendChild(document.createTextNode('Myndband ekki til'));
+
+    this.content.appendChild(document.createTextNode('Myndbandið sem beðið var um er ekki til.'));
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   const URL = document.URL.toString();
-
-
   console.log(URL);
-  if (URL.indexOf('localhost:3000/') == -1) {
+  if (URL.indexOf('localhost:3000/') !== -1) {
     const videoplayer = new VideoPlayer();
     videoplayer.load();
   } else {
